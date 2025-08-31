2a179d13982d2ca91e97bba81d083107853a6fc0
114514ns <lua6705@gmail.com>
2025-08-31 18:15:41 +0800

    1.更新React 2.使用React Compiler

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import tailwindcss from "@tailwindcss/vite"
+const ReactCompilerConfig = { /* ... */ };
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [    react({
